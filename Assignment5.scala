ec98e52ff1039daa96948d1404b3df81cbfafc62
Nikhil Patolia <34844354+nikhilpato@users.noreply.github.com>
2021-03-24 16:27:24 -0700

    Add files via upload

---
+++ Assignment5.scala
@@ -0,0 +1,17 @@
+import scala.io._
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import scala.collection._
+
+object Part1 {
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    System.setProperty("hadoop.home.dir", "c:/winutils/")
+
+    println("Hello, World!")
+  }
+}
